7b38d6be6ea57c4e7b79a41863465799338b32ca
Jesse Herrick <me@jesseherrick.com>
2020-06-23 04:47:04 -0400

    Finish building out rough, initial video calling abilities

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,29 +20,68 @@ import regeneratorRuntime from "regenerator-runtime"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
-var peerConnections = [];
-var localStream = null;
+var users = {}
+var peerConnections = []
+var localStream = null
 
-var createPeerConnection = (lv, targetUser) => {
+var addUserConnection = (userUuid) => {
+  if (users[userUuid] === undefined && userUuid != window.userUuid) {
+    users[userUuid] = {
+      peerConnection: null
+    }
+  }
+
+  return users;
+}
+
+var removeUserConnection = (userUuid) => {
+  delete users[userUuid]
+
+  return users
+}
+
+var createPeerConnection = (lv, fromUser, sdp) => {
   let newPeerConnection = new RTCPeerConnection({
     iceServers: [
       { urls: "stun:stun.l.google.com:19302" }
     ]
   })
+  users[fromUser].peerConnection = newPeerConnection;
 
-  // Add new peer connection to our peer connections array.
-  peerConnections.push(newPeerConnection)
+  // Add each local track to the RTCPeerConnection.
+  localStream.getTracks().forEach(track => newPeerConnection.addTrack(track, localStream))
+
+  // If creating an answer, rather than an initial offer.
+  if (sdp != undefined) {
+    newPeerConnection.setRemoteDescription({type: "offer", sdp: sdp})
+    newPeerConnection.createAnswer()
+      .then((answer) => { 
+        newPeerConnection.setLocalDescription(answer)
+        console.log("Sending this ANSWER to the requester:", answer)
+        lv.pushEvent("new_answer", {toUser: fromUser, description: answer})
+      })
+      .catch((err) => console.log(err))
+  }
 
   newPeerConnection.onicecandidate = async ({candidate}) => {
-    console.log(candidate, targetUser)
-    lv.pushEvent("send_ice_candidate", {candidate, targetUser})
+    // fromUser is the new value for toUser because we're sending this data back
+    // to the sender
+    lv.pushEvent("new_ice_candidate", {toUser: fromUser, candidate})
   }
 
   newPeerConnection.onnegotiationneeded = async () => {
     try {
-      await newPeerConnection.setLocalDescription(await newPeerConnection.createOffer())
-      let sdp = newPeerConnection.localDescription()
-      lv.pushEvent("send_video_offer", {targetUser, sdp})
+      newPeerConnection.createOffer()
+        .then((offer) => { 
+          newPeerConnection.setLocalDescription(offer)
+          console.log("Sending this offer to the requester:", offer)
+          lv.pushEvent("new_sdp_offer", {toUser: fromUser, description: offer})
+        })
+        .catch((err) => console.log(err))
+      // newPeerConnection.setLocalDescription(await newPeerConnection.createOffer())
+      // let description = newPeerConnection.localDescription
+      // console.log(description)
+      // lv.pushEvent("new_sdp_offer", {toUser: fromUser, description})
     }
     catch (error) { 
       console.log(error)
@@ -50,11 +89,9 @@ var createPeerConnection = (lv, targetUser) => {
   }
 
   newPeerConnection.ontrack = async (event) => {
-    document.getElementById(`video-remote-${targetUser}`).srcObject = event.streams[0]
+    console.log("Track received:", event)
+    document.getElementById(`video-remote-${fromUser}`).srcObject = event.streams[0]
   }
-
-  // Add each local track to the RTCPeerConnection.
-  // localStream.getTracks().forEach(track => newPeerConnection.addTrack(track, localStream))
 
   return newPeerConnection;
 }
@@ -70,7 +107,7 @@ Hooks.StartCall = {
     this.el.addEventListener("click", e => {
       // let myConnection = createPeerConnection()
       // Grabs local stream from the local computer.
-      window.navigator.mediaDevices.getUserMedia({audio: true, video: true})
+      navigator.mediaDevices.getUserMedia({audio: true, video: true})
         .then(stream => {
           localStream = stream;
           document.getElementById("local-video").srcObject = stream
@@ -98,6 +135,71 @@ Hooks.JoinCall = {
 
 Hooks.SetupPeer = {
 
+}
+
+Hooks.HandleOfferRequest = {
+  mounted () {
+    console.log("new offer request from", this.el.dataset.fromUserUuid)
+    let fromUser = this.el.dataset.fromUserUuid
+    // 1. Create peer connection with the requesting user.
+    // 2. Adds current tracks to the peer connection.
+    // 3. Adds the peer connection to the users array for the given user.
+    // 4. Creates an offer for the given peer connection.
+    // 5. Sends all offer and ICE candidate info to the requesting user via
+    //    the `new-offer` topic.
+    createPeerConnection(this, fromUser)
+  }
+}
+
+Hooks.HandleIceCandidateOffer = {
+  mounted () {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let iceCandidate = JSON.parse(data.iceCandidate)
+    let peerConnection = users[fromUser].peerConnection
+
+    console.log("new ice candidate from", fromUser, iceCandidate)
+
+    peerConnection.addIceCandidate(iceCandidate)
+  }
+}
+
+Hooks.HandleSdpOffer = {
+  mounted () {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+
+    if (sdp != "") {
+      console.log("new sdp OFFER from", data.fromUserUuid, data.sdp)
+      
+      createPeerConnection(this, fromUser, sdp)
+    }
+  }
+}
+
+Hooks.HandleAnswer = {
+  mounted () {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+    let peerConnection = users[fromUser].peerConnection
+
+    if (sdp != "") {
+      console.log("new sdp ANSWER from", fromUser, sdp)
+      peerConnection.setRemoteDescription({type: "answer", sdp: sdp})
+    }
+  }
+}
+
+Hooks.InitUser = {
+  mounted () {
+    addUserConnection(this.el.dataset.userUuid)
+  },
+
+  destroyed () {
+    removeUserConnection(this.el.dataset.userUuid)
+  }
 }
 
 let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
--- lib/littlechat_web/live/room/show_live.ex
+++ lib/littlechat_web/live/room/show_live.ex
@@ -14,9 +14,13 @@ defmodule LittlechatWeb.Room.ShowLive do
   @impl true
   def render(assigns) do
     ~L"""
-    <script>window.roomSlug = "<%= @room.slug %>"</script>
+    <script>
+    window.roomSlug = "<%= @room.slug %>";
+    </script>
 
     <h1><%= @room.title %></h1>
+
+    <p>Me: <%= @user.uuid %></p>
 
     <h3>Connected Users:</h3>
     <ul>
@@ -28,8 +32,33 @@ defmodule LittlechatWeb.Room.ShowLive do
     <video id="local-video" playsinline autoplay muted width="500"></video>
 
     <%= for uuid <- @connected_users do %>
-      <video id="video-remote-<%= uuid %>" playsinline autoplay width="500"></video>
+      <video id="video-remote-<%= uuid %>" data-user-uuid="<%= uuid %>" playsinline autoplay width="500" phx-hook="InitUser"></video>
     <% end %>
+
+    <div id="sdp-offers">
+      <%= for sdp_offer <- @sdp_offers do %>
+        <span phx-hook="HandleSdpOffer" data-from-user-uuid="<%= sdp_offer["from_user"] %>" data-sdp="<%= sdp_offer["description"]["sdp"] %>"><%= inspect sdp_offer %></span>
+      <% end %>
+    </div>
+
+    <div id="sdp-answers">
+      <%= for answer <- @answers do %>
+        <span phx-hook="HandleAnswer" data-from-user-uuid="<%= answer["from_user"] %>" data-sdp="<%= answer["description"]["sdp"] %>"></span>
+      <% end %>
+    </div>
+
+    <div id="ice-candidates">
+      <%= for ice_candidate_offer <- @ice_candidate_offers do %>
+        <span phx-hook="HandleIceCandidateOffer" data-from-user-uuid="<%= ice_candidate_offer["from_user"] %>" data-ice-candidate="<%= Jason.encode!(ice_candidate_offer["candidate"]) %>"><%= inspect ice_candidate_offer %></span>
+      <% end %>
+    </div>
+
+    <div id="offer-requests">
+      <%= inspect @offer_requests %>
+      <%= for request <- @offer_requests do %>
+        <span phx-hook="HandleOfferRequest" data-from-user-uuid="<%= request.from_user.uuid %>"></span>
+      <% end %>
+    </div>
 
     <div>
       <button phx-click="start_call" phx-hook="StartCall">Start Call</button>
@@ -57,19 +86,38 @@ defmodule LittlechatWeb.Room.ShowLive do
     {:ok,
       socket
       |> assign(:slug, slug)
-      |> assign(:user, create_connected_user())
+      |> assign(:user, user)
+      |> assign(:ice_candidate_offers, [])
+      |> assign(:sdp_offers, [])
+      |> assign(:answers, [])
+      |> assign(:offer_requests, [])
       |> assign(:connected_users, [])
       |> assign(:room, Organizer.get_room(slug))
     }
   end
 
   @impl true
-  def handle_event("start_call", _params, socket) do
+  def handle_event("start_call", params, socket) do
     {:noreply, socket}
   end
 
   @impl true
+  @doc """
+  Called when the user clicks the "Join Call" button. This sends a request to
+  each of the `@connected_users` with the event "request_offers".
+  """
   def handle_event("join_call", _params, socket) do
+    for user <- socket.assigns.connected_users do
+      send_direct_message(
+        socket.assigns.slug,
+        user,
+        "request_offers",
+        %{
+          from_user: socket.assigns.user
+        }
+      )
+    end
+
     {:noreply, socket}
   end
 
@@ -78,52 +126,68 @@ defmodule LittlechatWeb.Room.ShowLive do
     {:noreply, socket}
   end
 
+  @doc """
+  Passes along the ICE candidate sent by the JS callback to the user on the
+  other end. It also adds the current user's UUID to the message.
+  """
+  @impl true
+  def handle_event("new_ice_candidate", payload, socket) do
+    payload = Map.merge(payload, %{"from_user" => socket.assigns.user.uuid})
+    send_direct_message(socket.assigns.slug, payload["toUser"], "new_ice_candidate", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("new_sdp_offer", payload, socket) do
+    payload = Map.merge(payload, %{"from_user" => socket.assigns.user.uuid})
+
+    send_direct_message(socket.assigns.slug, payload["toUser"], "new_sdp_offer", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("new_answer", payload, socket) do
+    payload = Map.merge(payload, %{"from_user" => socket.assigns.user.uuid})
+
+    send_direct_message(socket.assigns.slug, payload["toUser"], "new_answer", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%Broadcast{event: "request_offers", payload: request}, socket) do
+    {:noreply,
+      socket
+      |> assign(:offer_requests, socket.assigns.offer_requests ++ [request])
+    }
+  end
 
   @doc """
-  Message sent from the client's JS once an offer.
+  See `handle_event("new_ice_candidate", params, socket)` for the sender of this
+  message. This function handles reception of a "new_ice_candidate" message and
+  tells the client about the ICE candidate.
   """
-  def handle_event("send_video_offer", %{"sdp" => sdp, "targetUser" => target_user}, socket) do
-    send_direct_message(
-      socket.assigns.slug,
-      target_user,
-      "incoming_video_offer",
-      %{
-        sdp: sdp,
-        from_user: socket.assigns.user,
-        to_user: target_user
-      }
-    )
-
-    {:noreply, socket}
+  @impl true
+  def handle_info(%Broadcast{event: "new_ice_candidate", payload: payload}, socket) do
+    {:noreply,
+      socket
+      |> assign(:ice_candidate_offers, socket.assigns.ice_candidate_offers ++ [payload])
+    }
   end
 
   @impl true
-  def handle_event("send_video_answer", %{"sdp" => sdp, "targetUser" => target_user}, socket) do
-    send_direct_message(
-      socket.assigns.slug,
-      target_user,
-      "send_video_answer",
-      %{
-        sdp: sdp,
-        from_user: socket.assigns.user,
-        to_user: target_user
-      }
-    )
+  def handle_info(%Broadcast{event: "new_sdp_offer", payload: payload}, socket) do
+    {:noreply,
+      socket
+      |> assign(:sdp_offers, socket.assigns.ice_candidate_offers ++ [payload])
+    }
   end
 
   @impl true
-  def handle_event("incoming_video_answer", %{}, socket) do
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info(%Broadcast{event: "incoming_video_offer", payload: %{sdp: sdp, from_user: from_user} = offer}, socket) do
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info(%Broadcast{event: "incoming_video_answer", payload: %{sdp: sdp, from_user: from_user} = offer}, socket) do
-    {:noreply, socket}
+  def handle_info(%Broadcast{event: "new_answer", payload: payload}, socket) do
+    {:noreply,
+      socket
+      |> assign(:answers, socket.assigns.answers ++ [payload])
+    }
   end
 
   @impl true
@@ -140,6 +204,7 @@ defmodule LittlechatWeb.Room.ShowLive do
   defp list_present(socket) do
     Presence.list("room:" <> socket.assigns.slug)
     |> Enum.map(fn {k, _} -> k end)
+    |> List.delete(socket.assigns.user.uuid)
   end
 
   defp send_direct_message(slug, to_user, event, payload) do
